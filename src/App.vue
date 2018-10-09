5f370805a2a1dfae42b90ff1aa2fbbf09ef92c25
VenenoFSD <venenofsd@qq.com>
2018-10-10 00:09:24 +0800

    left-list init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-
+        <div class="left-list"></div>
     </div>
 </template>
 
@@ -10,5 +10,19 @@
     }
 </script>
 
-<style>
+<style scoped>
+    #app {
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+    }
+    #app .left-list {
+        position: relative;
+        width: 230px;
+        height: 100%;
+        left: 0;
+        background-color: #eee;
+    }
 </style>
---
+++ src/components/left-list/LeftList.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "LeftList"
+    }
+</script>
+
+<style scoped>
+
+</style>
