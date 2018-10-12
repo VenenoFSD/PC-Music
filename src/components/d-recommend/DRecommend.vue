d10d9f86205054df1128e7bf0927f1cf54836eba
VenenoFSD <venenofsd@qq.com>
2018-10-13 02:05:33 +0800

    recommend new song finish

--- src/components/d-recommend/DRecommend.vue
+++ src/components/d-recommend/DRecommend.vue
@@ -11,13 +11,44 @@
         <div class="re-song-list-wrapper">
             <h2 class="title"><i class="iconfont icon-tuijian"></i>推荐歌单</h2>
             <separate></separate>
-            <ul class="re-song-list">
-                <li v-for="item in recommendSongList" :key="item.id" class="sl-item">
+            <ul class="list">
+                <li v-for="item in recommendSongList" :key="item.id" class="item sl-item">
                     <div class="img-wrapper">
-                        <img :src="item.picUrl" class="sl-img">
+                        <img :src="item.picUrl" class="img">
+                        <div class="playCount"><i class="iconfont icon-headset"></i><span>{{playCountFormat(item.playCount)}}</span></div>
+                        <div class="cover"></div>
                     </div>
                     <p class="text">{{item.name}}</p>
-                    <div class="playCount"><i class="iconfont icon-headset"></i><span>{{playCountFormat(item.playCount)}}</span></div>
+                </li>
+            </ul>
+        </div>
+        <div class="re-private-content-wrapper">
+            <h2 class="title"><i class="iconfont icon-cl-music"></i>独家放送</h2>
+            <separate></separate>
+            <ul class="list">
+                <li v-for="item in privateContent" class="item pc-item">
+                    <div class="img-wrapper">
+                        <img :src="item.sPicUrl" class="img">
+                        <div class="cover"></div>
+                    </div>
+                    <p class="text">{{item.name}}</p>
+                </li>
+            </ul>
+        </div>
+        <div class="re-new-song">
+            <h2 class="title"><i class="iconfont icon-cl-music"></i>最新音乐</h2>
+            <separate></separate>
+            <ul class="list">
+                <li v-for="(item, index) in newSong" class="item ns-item" :class="_nsItemClass(index)">
+                    <p class="num">{{_rankFormat(index)}}</p>
+                    <img :src="item.song.album.blurPicUrl" class="ns-img">
+                    <div class="desc">
+                        <p class="song-name-wrapper">
+                            <span class="song-name">{{item.name}}</span>
+                            <span class="song-desc" v-if="item.song.alias.length">（{{item.song.alias[0]}}）</span>
+                        </p>
+                        <p class="singer">{{item.song.artists[0].name}}</p>
+                    </div>
                 </li>
             </ul>
         </div>
@@ -36,6 +67,8 @@
             return {
                 banners: [],
                 recommendSongList: [],
+                privateContent: [],
+                newSong: [],
                 swiperOption: {
                     effect: 'coverflow',
                     centeredSlides: true,
@@ -85,16 +118,51 @@
                     let cutLen = len - 4;
                     return playCount.substr(0, cutLen) + '万';
                 }
+            },
+            getPrivateContent () {
+                axios.get(`http://localhost:3000/personalized/privatecontent`).then((res) => {
+                    if (res.data && res.data.code === 200) {
+                        this.privateContent = res.data.result;
+                    }
+                });
+            },
+            getNewSong () {
+                axios.get(`http://localhost:3000/personalized/newsong`).then((res) => {
+                    if (res.data && res.data.code === 200) {
+                        this.newSong = res.data.result;
+                    }
+                });
+            },
+            _nsItemClass (item) {
+                if (item === 0 || item === 4 || item === 8) {
+                    return 'bgc border';
+                } else if (item === 2 || item === 6) {
+                    return 'border';
+                } else if (item === 1 || item === 5 || item === 9) {
+                    return 'bgc';
+                } else {
+                    return '';
+                }
+            },
+            _rankFormat (index) {
+                if (index < 9) {
+                    return '0' + (index + 1);
+                } else {
+                    return index + 1;
+                }
             }
         },
         created () {
             this.getBanner();
             this.getRecommendSongList();
+            this.getPrivateContent();
+            this.getNewSong();
         },
         computed: {
             showSwiper () {
                 return this.banners.length;
             }
+
         },
         components: {
             swiper,
@@ -106,8 +174,9 @@
 
 <style scoped>
     .d-recommend {
-        max-width: 1300px;
+        max-width: 1200px;
         margin: 0 auto;
+        transform: translate3d(8px, 0, 0);
     }
     .banner-wrapper {
         margin: 30px 0 50px 0;
@@ -134,55 +203,110 @@
     .banner-wrapper >>> .swiper-pagination-bullet-active {
         background-color: #ec0000;
     }
-    .re-song-list {
+    .list {
         display: flex;
-        max-width: 1300px;
-        margin: 12px auto;
+        margin-top: 12px;
         overflow: hidden;
         justify-content: space-between;
         flex-wrap: wrap;
     }
-    .re-song-list .sl-item {
-        width: 18%;
-        flex: 0 0 18%;
-        margin-bottom: 30px;
+    .list .item .img-wrapper {
         position: relative;
+        font-size: 0;
     }
-    .re-song-list .sl-item:hover {
-        opacity: 0.8;
+    .list .item .img-wrapper .cover {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        z-index: 1;
+        left: 0;
+        top: 0;
+        background-color: rgba(0,0,0,.3);
+        opacity: 0;
+        transition: opacity 0.6s;
     }
-    .re-song-list .sl-item .sl-img {
+    .list .item .img-wrapper:hover .cover {
+        opacity: 1;
+    }
+    .list .item .img {
         width: 100%;
     }
-    .re-song-list .sl-item .text {
+    .list .sl-item {
+        width: 18.6%;
+        flex: 0 0 18.6%;
+        margin-bottom: 30px;
+    }
+    .list .pc-item {
+        width: 32%;
+        flex: 0 0 32%;
+    }
+    .list .ns-item {
+        width: 50%;
+        flex: 0 0 50%;
+        height: 64px;
+        display: flex;
+        align-items: center;
+    }
+    .list .ns-item.bgc {
+        background-color: #f4f4f6;
+    }
+    .list .ns-item.border {
+        border-right: 1px solid #ddd;
+        box-sizing: border-box;
+    }
+    .list .ns-item .num {
+        width: 40px;
+        text-align: center;
+    }
+    .list .ns-item .ns-img {
+        width: 54px;
+        height: 54px;
+        margin-right: 14px;
+    }
+    .list .ns-item .desc .song-name-wrapper {
+        font-size: 16px;
+        margin-bottom: 4px;
+    }
+    .list .ns-item .desc .song-name-wrapper .song-desc {
+        color: #aaa;
+    }
+    .list .ns-item .desc .singer {
+        font-size: 12px;
+        color: #aaa;
+    }
+    .list .item .text {
         white-space: nowrap;
         text-overflow: ellipsis;
         overflow: hidden;
         margin-top: 6px;
         font-size: 15px;
     }
-    .re-song-list .sl-item .playCount {
+    .list .item .img-wrapper .playCount {
         position: absolute;
+        z-index: 2;
         right: 0;
         top: 0;
         font-size: 12px;
         padding: 1px 4px 0 2px;
         color: #fff;
-        background: linear-gradient(to right, transparent , rgba(0,0,0,.2));;
+        background: linear-gradient(to right, transparent , rgba(0,0,0,.2));
     }
-    .re-song-list .sl-item .playCount .iconfont {
+    .list .item .img-wrapper .playCount .iconfont {
         vertical-align: top;
         margin-right: 2px;
     }
-    .re-song-list .sl-item .playCount span {
+    .list .item .img-wrapper .playCount span {
         vertical-align: top;
     }
     .title {
-        font-size: 16px;
-        margin-bottom: 16px;
+        font-size: 15px;
+        margin-bottom: 15px;
     }
     .title .iconfont {
         color: #c70000;
         margin-right: 6px;
+    }
+    .re-private-content-wrapper {
+        margin: 20px 0 50px 0;
     }
 </style>
