4b27ea56bac217b8d1e6edd4753662af7836ba34
VenenoFSD <venenofsd@qq.com>
2018-10-14 17:13:40 +0800

    add load component

---
+++ src/base/load/Load.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="load">
+        <img src="./loading.gif">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Load"
+    }
+</script>
+
+<style scoped>
+    .load {
+        font-size: 0;
+        text-align: center;
+        margin-top: 250px;
+    }
+    .load img {
+        width: 50px;
+    }
+</style>
