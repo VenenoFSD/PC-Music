1615e84e7c7a9a0d7d907dd60f9eb6f9e3f685bd
VenenoFSD <venenofsd@qq.com>
2018-10-15 01:32:23 +0800

    lazyload configure

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import VueLazyLoad from 'vue-lazyload'
 import './common/css/base.css'
 import './common/css/iconfont.css'
 
 Vue.config.productionTip = false;
+
+Vue.use(VueLazyLoad, {
+    loading: require('./common/imgs/lazy.png')
+});
 
 /* eslint-disable no-new */
 new Vue({
